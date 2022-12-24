3af979c5b1bfc71b87d6ef103c75b22014bd8a83
Hiren205 <aswarhiren93@gmail.com>
2022-12-24 06:08:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors=require("cors");
+
+
+const app = express();
+
+const user = require("./models/user");
+
+
+app.use(express.json());
+app.use(cors());
+mongoose.connect("mongodb+srv://hiren123:hiren123@cluster0.unp4crn.mongodb.net/noteflutter");
+app.get("/note/list", async (req, res) => {
+  const data = await user.find({ userid: req.body.userid });
+
+  res.json(data);
+});
+app.post("/note/add", async (req, res) => {
+  console.log(req.body);
+  const data = new user({
+    id: req.body.id,
+    userid: req.body.userid,
+    title: req.body.title,
+    content: req.body.content,
+  });
+  const result = await data.save();
+  res.json(result);
+});
+app.post("/note/delet", async (req, res) => {
+  const data = await user.deleteOne({ id: req.body.id });
+   res.json(data);
+});
+const PORT=process.env.PORT||3001;
+app.listen(PORT, () => {
+  console.log("server is listing");
+});
